93385403d2ccdc30f3d02895dc44e3f5d7322010
alex <bill666linux@gmail.com>
2022-01-08 11:27:54 +0800

    add ImagesActivity and jsoup to dependency

---
+++ app/src/main/java/com/example/team9picturematchca/ImagesActivity.java
@@ -0,0 +1,13 @@
+package com.example.team9picturematchca;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_images);
+    }
+}
