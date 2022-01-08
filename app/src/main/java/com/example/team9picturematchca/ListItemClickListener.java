5fdb66e82ebaf500b079bf746f886b03a39dc169
Weili44 <e0838432@u.nus.edu>
2022-01-08 10:57:47 +0800

    ListItem & RecyclerAdapter

---
+++ app/src/main/java/com/example/team9picturematchca/ListItemClickListener.java
@@ -0,0 +1,6 @@
+package com.example.team9picturematchca;
+
+public interface ListItemClickListener {
+    // created interface for items in List
+    void onListItemClick(int position);
+}
---
+++ app/src/main/java/com/example/team9picturematchca/RecyclerAdapter.java
@@ -0,0 +1,104 @@
+package com.example.team9picturematchca;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder>{
+
+    ArrayList<Bitmap> imgs;
+    HashMap<Integer, BitmapDrawable> selected;
+    final private Context context;
+    final private ListItemClickListener onClickListener;
+
+    public RecyclerAdapter(Context context){
+        this.context = context;
+        imgs = new ArrayList<>();
+        selected = new HashMap<>();
+        this.onClickListener = (ListItemClickListener) context;
+    }
+
+    public void AddImage(String url, Bitmap bitmap){
+        imgs.add(bitmap);
+        // indicates that any reflection of the data at position is out of date and should be updated
+        this.notifyItemChanged(imgs.size() - 1);
+    }
+
+    public ArrayList<BitmapDrawable> getSelectedImgs(){
+        return new ArrayList<>(selected.values());
+    }
+
+    public void clearImages(){
+        imgs.clear();
+        selected.clear();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        // row_item layout to be created
+        View view = layoutInflater.inflate(R.layout.row_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerAdapter.ViewHolder holder, int position) {
+        ImageView imageView = holder.imageView; // in row_item layout
+        ImageView checkBox = holder.checkBox; // in row_item layout
+
+        imageView.setImageBitmap(imgs.get(position));
+
+        if(selected.containsKey(position)){
+            // to create a green border in drawable folder
+            imageView.setBackground(ContextCompat.getDrawable(context, R.drawable.green_border));
+            checkBox.setVisibility(View.VISIBLE);
+        } else{
+            imageView.setBackground(null);
+            checkBox.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return imgs.size(); // no. of rows in Recycler View
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        ImageView imageView;
+        ImageView checkBox;
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.imageView); // in row_item layout
+            checkBox = itemView.findViewById(R.id.checkBox); // in row_item layout
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            if(selected.containsKey(position)){
+                selected.remove(position);
+            } else{
+                selected.put(position, (BitmapDrawable) imageView.getDrawable());
+            }
+            onClickListener.onListItemClick(position);
+        }
+    }
+
+}
