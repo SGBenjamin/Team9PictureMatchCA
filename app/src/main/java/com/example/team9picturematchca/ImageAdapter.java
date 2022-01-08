d17df4551d35f3e9594c4510f1e94a586f02ba67
Tat Sheng Benjamin Chan <Benjamin Chan>
2022-01-08 11:36:59 +0800

    some changes to ImageAdapter.java and populated activity_image_configuration.xml layout.

--- app/src/main/java/com/example/team9picturematchca/ImageAdapter.java
+++ app/src/main/java/com/example/team9picturematchca/ImageAdapter.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,51 +17,74 @@ import java.util.List;
 
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
 
+    private boolean useGlide;
     private List<MatchImage> matchImages;
 
-    public ImageAdapter(List<MatchImage> matchImages) {
+    public ImageAdapter(boolean useGlide, List<MatchImage> matchImages) {
+        this.useGlide = useGlide;
         this.matchImages = matchImages;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
-        private final ImageView imageView;
+    public interface OnItemClickListener {
+        void onItemClick(View itemView, int position);
+    }
 
-        public ViewHolder(View view){
-            super(view);
+    private OnItemClickListener listener;
 
-            imageView = (ImageView) view.findViewById(R.id.imageView);
-        }
-        public ImageView getImageView(){
-            return imageView;
+    public void setOnItemClickListener(OnItemClickListener listener) {//Check with the rest on what is this block of code for
+        this.listener = listener;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imageView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.imageView);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        listener.onItemClick(itemView, position);
+                    }
+                }
+            });
         }
     }
 
-    public void CustomAdapter (List<MatchImage> imageSet){//Check with the rest on what is this block of code for
-        matchImages = imageSet;
-    }
+
     @NonNull
     @Override
     //This is correct based off https://developer.android.com/guide/topics/ui/layout/recyclerview
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
         Context context = viewGroup.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
-        View itemView = inflater.inflate(R.layout.game_row_item, viewGroup, false);
+        View itemView = inflater.inflate(R.layout.activity_image_configuration, viewGroup, false);
         return new ViewHolder(itemView);
     }
+
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, int position) {
         MatchImage matchImage = matchImages.get(position);
         ImageView imageView = viewHolder.imageView;
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
-        Glide.with(imageView
+        if (useGlide) {
+            Glide.with(imageView
                     .getContext())
-                    .load(matchImage.getImgPath())
+                    .load(matchImage.getImgPath())//this is where the URL will be accessed from
                     .into(imageView);
+        } else {
+            Bitmap bitmap = BitmapFactory.decodeFile(matchImage.getImgPath());
+            imageView.setImageBitmap(bitmap);
+        }
     }
     @Override
     public int getImageCount(){
-        return matchImages.length;
+        return matchImages.size();
     }
 
 }
