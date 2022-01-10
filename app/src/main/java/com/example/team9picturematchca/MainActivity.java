b100ed78a62eb622e8957bf01100679149d2903c
Liu Fang <e0843571@u.nus.edu>
2022-01-10 13:54:02 +0800

    Add the "About" button and webview

--- app/src/main/java/com/example/team9picturematchca/MainActivity.java
+++ app/src/main/java/com/example/team9picturematchca/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.team9picturematchca;
 
 import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String EXTERNAL_URL = "externalUrl";
 
     Button multiMatch;
     Button soloMatch;
@@ -38,7 +42,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         highScores = findViewById(R.id.highscoreBtn);
         highScores.setOnClickListener(this);
+
+        Button abtT9Btn = findViewById(R.id.abtT9Btn);
+        abtT9Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String externalUrl =
+                        "https://www.iss.nus.edu.sg/graduate-programmes/programme/detail/graduate-diploma-in-systems-analysis";
+                launchExternalPage(externalUrl);
+            }
+        });
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        // Interrupt before switching to menu
+////        if (imageProcess != null) {
+////            imageProcess.interrupt();
+////        }
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.abtApp) {
+////            String externalUrl =
+////                    "https://www.iss.nus.edu.sg/graduate-programmes/programme/detail/graduate-diploma-in-systems-analysis";
+//            Intent intent = new Intent(this, WebViewActivity.class);
+////            intent.putExtra(EXTERNAL_URL, externalUrl);
+//            startActivity(intent);
+//        }
+//
+////        if (id == R.id.highscore) {
+////            Intent intent = new Intent(this, HighscoreActivity.class);
+////            startActivity(intent);
+////        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
 
 
@@ -66,6 +117,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(getApplicationContext(), HighScoreActivity.class);
             startActivity(intent);
         }
+    }
+
+    private void launchExternalPage(String externalUrl) {
+        Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+        intent.putExtra(EXTERNAL_URL, externalUrl);
+        startActivity(intent);
     }
 
     // trying to push
